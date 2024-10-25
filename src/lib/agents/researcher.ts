e0a8e934334e8c243e183c03e24d2741ad864b54
ritikraj1 <ritikraj240@gmail.com>
2024-10-25 23:38:54 +0530

    blog word limit increased

--- src/lib/agents/researcher.ts
+++ src/lib/agents/researcher.ts
@@ -8,10 +8,10 @@ import { ok, err, Result } from "true-myth/result"
 // If there are any images relevant to your answer, be sure to include them as well.
 // Aim to directly address the user's question, augmenting your response with insights gleaned from the search results.`;
 
-const SYSTEM_PROMPT = `As a professional seo expert, your task is to come up with a detailed outline for an SEO blog post given a user query.
+const SYSTEM_PROMPT = `As a professional seo expert, your task is to come up with a detailed outline for an SEO blog post for a client given a blog post topic and client details.
 
 You possess the ability to search for any information on the web.
-For each user query, utilize the search results to their fullest potential to provide additional information and assistance in your response.
+For each blog post topic, utilize the search results to their fullest potential to provide additional information and assistance in your response.
 
 Then, based on your outline created from the initial search, conduct further research on all the sub-topics to enrich the outline. If you are writing individual products or services, be sure to research them as well. 
 
@@ -19,19 +19,20 @@ For the outline, make sure each sections has all the relevant external links and
 
 Here are a few rules to follow for the outline:
 
-  1) Make sure to provide a strong hook, intro to the blog post.
-  2) Pay particular attention to crafting the introduction. The introduction should provide value instantly and mention about the pain point of the audience.
-  3) Follow the Problem - Agitation - Solution copy writing framework. It's important that you generally follow and embed this flow but not explicitly mention it.
-  4) The outline should contain a main image below the title.
-  5) Make sure to add images and external links for subtopics. Make sure to preserve/add as many external urls and images as possible.
-  6) Instead of generating simple bullet points, if you have found a relevant information from the web, include it in the outline in a detail manner so that it retains as much meaningful information as possbile.
-  7) If relevant, make sure to compare and contrast products and services.
-  8) Again, make sure to conduct the initial research, come up with a basic outline, then conduct additional research on all the subtopics to enrich the outline.
+  1) Word count of the final blog post must be greater than 3000 words, so strictly provide sufficient number of sections, sub-sections, images and links in the outline considering this.
+  2) Make sure to provide a strong hook, intro to the blog post.
+  3) Pay particular attention to crafting the introduction. The introduction should provide value instantly and mention about the pain point of the audience.
+  4) Use the client details provided in the input and web search results to promote client and its business/service in the blog post by following the Problem - Agitation - Solution copy writing framework. It's important that you generally follow and embed this flow but not explicitly mention it.
+  5) The outline should contain a main image below the title.
+  6) Make sure to add images and external links for subtopics. Make sure to preserve/add as many external urls and images as possible.
+  7) Instead of generating simple bullet points, if you have found a relevant information from the web, include it in the outline in a detail manner so that it retains as much meaningful information as possbile.
+  8) If relevant, make sure to compare and contrast products and services.
+  9) Again, make sure to conduct the initial research, come up with a basic outline, then conduct additional research on all the subtopics to enrich the outline.
   `;
 
 export async function researcher(query: string): Promise<Result<string,string>> {
   try {
-    let toolResults: any[] = [];
+    // let toolResults: any[] = [];
 
     const currentDate = new Date().toLocaleString();
     const result = await generateText({
@@ -40,6 +41,7 @@ export async function researcher(query: string): Promise<Result<string,string>> 
       prompt: query,
       tools: getTools(),
       maxSteps: 10,
+      maxTokens: 7000,
       // onStepFinish: async (event: {
       //   stepType: string;
       //   toolCalls?: any[];
--- src/lib/agents/writer.ts
+++ src/lib/agents/writer.ts
@@ -9,15 +9,16 @@ import { Result, ok, err } from "true-myth/result";
 
 const SYSTEM_PROMPT = `As a professional SEO blog writer, you will be given an detailed outline for an SEO blog post.
 
-Your task is to write a blog post based on the outline. that is around 2000 words long.
+Your task is to write a blog post, of length strictly more than 3000 words, based on the outline.
 
 Here are a couple things to note when writing a blog post:
-1) Make sure to provide a strong hook, intro to the blog post.
-2) Pay particular attention to crafting the introduction. The introduction should provide value instantly and mention about the pain point of the audience.
-3) Follow the Problem - Agitation - Solution copy writing framework. It's important that you generally follow and embed this flow but not explicitly mention it.
-4) Don't use over complex languages. Make sure it's around 8th grade reading level in a natural tone. 
-5) Preserve all images in the correct place.
-6. Make sure external links are embedded naturally with the right anchor text throughout the article.
+1) Word count of the blog post must be greater than 3000 words. Follow this very strictly.
+2) Make sure to provide a strong hook, intro to the blog post.
+3) Pay particular attention to crafting the introduction. The introduction should provide value instantly and mention about the pain point of the audience.
+4) Follow the Problem - Agitation - Solution copy writing framework. It's important that you generally follow and embed this flow but not explicitly mention it.
+5) Don't use over complex languages. Make sure it's around 8th grade reading level in a natural tone. 
+6) Preserve all images in the correct place.
+7) Make sure external links are embedded naturally with the right anchor text throughout the article.
   `;
 
 export async function writer(outline: string): Promise<Result<string,string>> {
@@ -27,7 +28,7 @@ export async function writer(outline: string): Promise<Result<string,string>> {
       model: getModel(),
       system: `${SYSTEM_PROMPT} Current date and time: ${currentDate}`,
       prompt: outline,
-      maxTokens: 5000,
+      maxTokens: 7000,
     });
 
     console.log("Result: ", result);
