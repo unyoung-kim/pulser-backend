3ceb6385845f42154444fb92aa775898b7cbbba1
Unyoung Kim <uykim0005@gmail.com>
2024-10-30 00:32:58 -0400

    improved researcher

--- src/lib/workflow.ts
+++ src/lib/workflow.ts
@@ -1,15 +1,15 @@
-import { Result, ok, err } from "true-myth/result";
-import { SupabaseClient } from '@supabase/supabase-js'
+import { SupabaseClient } from "@supabase/supabase-js";
+import { Result, err, ok } from "true-myth/result";
+import { outlineEnricher } from "./agents/outline-enricher.js";
 import { querySuggestor } from "./agents/query-suggestor.js";
 import { researcher } from "./agents/researcher.js";
 import { writer } from "./agents/writer.js";
-import { outlineEnricher } from "./agents/outline-enricher.js";
-import { getSupabaseClient } from "./get-supabase-client.js";
 import { EnrichedURL } from "./enrich-internal-links.js";
+import { getSupabaseClient } from "./get-supabase-client.js";
 
-export interface WorkflowResult{
-  answer: string,
-  relatedQueries: { query: string }[]
+export interface WorkflowResult {
+  answer: string;
+  relatedQueries: { query: string }[];
 }
 
 /**
@@ -20,7 +20,13 @@ export interface WorkflowResult{
  * @param query
  * @returns
  */
-export async function workflow({ projectId, inputTopic }: { projectId: string; inputTopic?: string}): Promise<Result<WorkflowResult, string>> {
+export async function workflow({
+  projectId,
+  inputTopic,
+}: {
+  projectId: string;
+  inputTopic?: string;
+}): Promise<Result<WorkflowResult, string>> {
   // const action = (await taskManager(query)) ?? { object: { next: 'proceed' } };
 
   // if (action.object.next === 'inquire') {
@@ -30,58 +36,61 @@ export async function workflow({ projectId, inputTopic }: { projectId: string; i
 
   //
 
-  const topic = inputTopic ?? "Topic generated from topic generator"  // Leaving it as it is for now as we still need to figure out how to generate keywords
+  const topic = inputTopic ?? "Topic generated from topic generator"; // Leaving it as it is for now as we still need to figure out how to generate keywords
 
-  const outline: Result<string,string> = await researcher(topic);
+  const outline: Result<string, string> = await researcher(topic);
 
-  if(outline.isErr){
-    return err(outline.error)
+  if (outline.isErr) {
+    return err(outline.error);
   }
 
   console.log("Outline: ", outline.value);
 
+  const supabaseClient: Result<SupabaseClient, string> = getSupabaseClient();
 
-  const supabaseClient: Result<SupabaseClient,string> = getSupabaseClient();
-
-  if(supabaseClient.isErr){
+  if (supabaseClient.isErr) {
     return err(supabaseClient.error);
   }
 
-  const supabase= supabaseClient.value;
+  const supabase = supabaseClient.value;
 
   const enrichedURLsResponse = await supabase
-    .from('InternalLink')
-    .select('url,summary')
-    .eq('project_id',projectId)
+    .from("InternalLink")
+    .select("url,summary")
+    .eq("project_id", projectId);
 
-    const enrichedURLs: EnrichedURL[] = (enrichedURLsResponse.data ?? []).map(item => ({
+  const enrichedURLs: EnrichedURL[] = (enrichedURLsResponse.data ?? []).map(
+    (item) => ({
       id: item.url,
       summary: item.summary,
-  }));
+    })
+  );
 
-  const enrichedOutline: Result<string,string> = await outlineEnricher(enrichedURLs, outline.value);
+  const enrichedOutline: Result<string, string> = await outlineEnricher(
+    enrichedURLs,
+    outline.value
+  );
 
-  if(enrichedOutline.isErr){
-    return err(enrichedOutline.error)
+  if (enrichedOutline.isErr) {
+    return err(enrichedOutline.error);
   }
 
   console.log("Enriched outline: ", enrichedOutline.value);
 
+  const article: Result<string, string> = await writer(enrichedOutline.value);
 
-  const article: Result<string,string> = await writer(enrichedOutline.value);
-
-  if(article.isErr){
-    return err(article.error)
+  if (article.isErr) {
+    return err(article.error);
   }
 
   console.log("Article: ", article.value);
-  
 
-  const relatedQueries: Result<{ query: string }[],string> = await querySuggestor(topic);
+  const relatedQueries: Result<{ query: string }[], string> =
+    await querySuggestor(topic);
 
-  if(relatedQueries.isErr){
-    return err(relatedQueries.error)
+  if (relatedQueries.isErr) {
+    return err(relatedQueries.error);
   }
 
-  return ok({answer: article.value, relatedQueries: relatedQueries.value});
+  return ok({ answer: article.value, relatedQueries: relatedQueries.value });
 }
