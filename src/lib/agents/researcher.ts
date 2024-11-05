c2ff4b9777069311e7431c4234443d1dcab5f062
ritikraj1 <ritikraj240@gmail.com>
2024-11-05 21:42:18 +0530

    resolved conflict with main

--- src/lib/agents/researcher.ts
+++ src/lib/agents/researcher.ts
@@ -78,29 +78,26 @@ Conduct a deep research on the topic. Then, come up with the initial outline of 
 For the outline, make sure each sections has all the relevant external links and images associated in the outline. Output the outline inside <outline> tags.
 
 Here are a few rules you must follow for the outline:
-
-  1) Provide sufficient number of sections, sub-sections, images and links in the outline for a very long blog post.
-  2) Make sure to provide a strong hook, intro to the blog post. The introduction should provide value instantly and mention about the pain point of the audience.
-  3) Use the client details provided in the input and web search results to promote client and its business/service in the blog post by following the Problem - Agitation - Solution copy writing framework. It's important that you generally follow and embed this flow but not explicitly mention it.
-  4) The outline should contain a main image below the title. 
-  5) If relevant, make sure to compare and contrast products and services.
-  6) For each section, add actual images and as many real links from the research in the format of [Link: ...] and [Image: ...]
-`;
+1) Provide sufficient number of sections, sub-sections, images, and links in the outline for a very long blog post.
+2) Make sure to provide a strong hook, intro to the blog post. The introduction should provide value instantly and mention about the pain point of the audience.
+3) Use the client details provided in the input and web search results to promote client and its business/service in the blog post by following the Problem - Agitation - Solution copy writing framework. It's important that you generally follow and embed this flow but not explicitly mention it.
+4) The outline should contain a main image below the title. 
+5) If relevant, make sure to compare and contrast products and services.
+6) For each section, add actual images and as many real links from the research in the format of [Link: ...] and [Image: ...]
+7) Additionally, include relevant videos wherever applicable from the research in the format of [Video: ...].`;
 
 const FINAL_OUTLINE_PROMPT = `You will be given an outline of an SEO blog post.
 
 You possess the ability to search for any information on the web.
 
-Your task is to conduct further research on sub topics and enrich the outline by adding more details, images and links. Find where resources are lacking and conduct further research on those topics. Only return the outline with the additional research and don't include any explanation text about the task. Output the outline inside <outline> tags.
+Your task is to conduct further research on subtopics and enrich the outline by adding more details, images, videos, and links. Find where resources are lacking and conduct further research on those topics. Only return the outline with the additional research and don't include any explanation text about the task. Output the outline inside <outline> tags.
 
 Here are a few rules you must follow:
-
-1) Make sure introduction has multiple links. The introduction should provide value instantly and mention about the pain point of the audience. Make sure when conducting research to find some unique insight about the topic like a quote, statistic, or a surprising fact and add it to the outline with the link.
+1) Make sure the introduction has multiple links. The introduction should provide value instantly and mention about the pain point of the audience. Make sure when conducting research to find some unique insight about the topic like a quote, statistic, or a surprising fact and add it to the outline with the link.
 2) The outline should follow the Problem - Agitation - Solution copy writing framework. It's important that you generally follow and embed this flow but not explicitly mention it.
 3) Make sure to add images and links for subtopics that lack them in the given outline. You will be rewarded extra points for having multiple links per subtopic.
-4) Preserve all of images and links previously provided in the outline. If you have found detailed and useful information, enrich the outline with it. 
-5) Return a full outline with all the sections, sub-sections, images and links without any ommissions from the given outline.
-`;
+4) Preserve all of the images, videos, and links previously provided in the outline. If you have found detailed and useful information, enrich the outline with it. 
+5) Return a full outline with all the sections, sub-sections, images, videos, and links without any omissions from the given outline.`;
 
 /**
  * This is a sequential approach to the researcher agent. This is a more deterministic approach and it also preserves more information from the initial research.
--- src/lib/agents/writer.ts
+++ src/lib/agents/writer.ts
@@ -7,17 +7,16 @@ import { getModel } from "../get-model.js";
 // If there are any images relevant to your answer, be sure to include them as well.
 // Aim to directly address the user's question, augmenting your response with insights gleaned from the search results.`;
 
-const SYSTEM_PROMPT = `As a professional SEO blog writer, you will be given an detailed outline for an SEO blog post.
+const SYSTEM_PROMPT = `As a professional SEO blog writer, you will be given a detailed outline for an SEO blog post.
 
 Your task is to write a blog post, of length strictly more than 3000 words, based on the outline.
 
-Here are a couple things to note when writing a blog post:
+Here are a couple of things to note when writing a blog post:
 1) Word count of the blog post must be greater than 3500 words. Follow this very strictly.
-2) Use all the images and links provided in the outline. Links should be embedded naturally with the right anchor text throughout the article.
+2) Use all the images, videos, and links provided in the outline. Links should be embedded naturally with the right anchor text throughout the article.
 3) Pay particular attention to crafting the introduction. The introduction should provide value instantly and mention about the pain point of the audience. Keep the sentences short and concise.
 4) Follow the Problem - Agitation - Solution copy writing framework. It's important that you generally follow and embed this flow but not explicitly mention it.
-5) Don't use over complex languages. Make sure it's around 8th grade reading level in a natural tone. Make this less cheesy and sound natural with simple sentence structures and words.
-  `;
+5) Don't use overly complex language. Make sure it's around 8th-grade reading level in a natural tone. Make this less cheesy and sound natural with simple sentence structures and words.`;
 
 export async function writer(outline: string): Promise<Result<string, string>> {
   try {
