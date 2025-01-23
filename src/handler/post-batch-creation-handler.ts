d10cb628f238440abc5bc241a1d52f599a48795f
ritikraj1 <ritikraj240@gmail.com>
2025-01-24 01:29:27 +0530

    added mail sending in batch creation

--- src/handler/post-batch-creation-handler.ts
+++ src/handler/post-batch-creation-handler.ts
@@ -3,6 +3,7 @@ import { workflowV3 } from "../lib/articles/workflowV3/workflowV3.js";
 import { ApiResponseSchema } from "../lib/schema/api-response-schema.js";
 import { tRPC } from "../lib/trpc.js";
 import { z } from "zod";
+import { sendEmail } from "../lib/utility/send-email.js";
 
 export function postBatchCreationHandler(t: tRPC, path: string) {
   return t.procedure
@@ -27,34 +28,43 @@ export function postBatchCreationHandler(t: tRPC, path: string) {
           secondaryKeywords: z.array(z.array(z.string())).optional(),
           instructions: z.array(z.string()).optional(),
         }),
+        email: z.string(),
       })
     )
     .output(ApiResponseSchema)
     .mutation(async ({ input }) => {
       try {
-        console.log("TYPE: ", input.type);
-        input.type === "NORMAL"
-          ? Promise.all(
-              input.arguments.inputTopics.map(async (inputTopic, index) => {
-                return await workflowV3({
-                  projectId: input.projectId,
-                  inputTopic: inputTopic,
-                  keywordId: input.arguments.keywordIds[index],
-                  secondaryKeywords: input.arguments.secondaryKeywords?.[index],
-                  instruction: input.arguments.instructions?.[index],
-                  length: input.arguments.lengths[index],
-                });
-              })
-            )
-          : Promise.all(
-              input.arguments.inputTopics.map(async (inputTopic, index) => {
-                return await glossaryWorkflow({
-                  projectId: input.projectId,
-                  inputTopic: inputTopic,
-                  keywordId: input.arguments.keywordIds[index],
-                });
-              })
-            );
+        (async () => {
+          console.log("TYPE: ", input.type);
+          input.type === "NORMAL"
+            ? await Promise.all(
+                input.arguments.inputTopics.map(async (inputTopic, index) => {
+                  return await workflowV3({
+                    projectId: input.projectId,
+                    inputTopic: inputTopic,
+                    keywordId: input.arguments.keywordIds[index],
+                    secondaryKeywords:
+                      input.arguments.secondaryKeywords?.[index],
+                    instruction: input.arguments.instructions?.[index],
+                    length: input.arguments.lengths[index],
+                  });
+                })
+              )
+            : await Promise.all(
+                input.arguments.inputTopics.map(async (inputTopic, index) => {
+                  return await glossaryWorkflow({
+                    projectId: input.projectId,
+                    inputTopic: inputTopic,
+                    keywordId: input.arguments.keywordIds[index],
+                  });
+                })
+              );
+          await sendEmail(
+            input.email,
+            "Batch post creation completed",
+            "Hi, your batch post creation is completed. Thanks and Regards, Pulser AI"
+          );
+        })();
         return {
           success: true,
           data: "Posts creation batched",
--- src/lib/utility/send-email.ts
+++ src/lib/utility/send-email.ts
@@ -1,8 +1,7 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY ?? "");
-
 export const sendEmail = async (to: string, subject: string, html: string) => {
+  const resend = new Resend(process.env.RESEND_API_KEY ?? "");
   await resend.emails.send({
     from: "team@pulserseo.com",
     to,
