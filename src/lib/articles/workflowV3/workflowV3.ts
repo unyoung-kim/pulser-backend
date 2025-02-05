9fb394f6fab7a37501c549545c01818d673c16f1
ritikraj1 <ritikraj240@gmail.com>
2025-02-05 21:11:45 +0530

    generating and error status included in workflowV3 and glossary

--- src/lib/articles/workflowV3/workflowV3.ts
+++ src/lib/articles/workflowV3/workflowV3.ts
@@ -5,7 +5,8 @@ import { fineTunedWriter, ftWriterEnhancer } from "../../agents/writer-ft.js";
 import { getSupabaseClient } from "../../get-supabase-client.js";
 import { EnrichedURL } from "../../internal-link/enrich-internal-links.js";
 import { incrementUsageCredit } from "../../supabase/usage.js";
-
+import { getClerkEmailId } from "../../utility/get-clerk-email-id.js";
+import { sendEmail } from "../../utility/send-email.js";
 /**
  * Based on user query, generate a blog post
  *
@@ -37,107 +38,11 @@ export async function workflowV3({
 
   const supabase = supabaseClient.value;
 
-  const { data: project, error: projectError } = await supabase
-    .from("Project")
-    .select("name,domain,background,description,org_id")
-    .eq("id", projectId);
-
-  if (projectError) {
-    return err(`Error fetching project details: ${projectError.message}`);
-  }
-
-  const projectName: string | null = project?.at(0)?.name ?? null;
-
-  const projectBackground: Record<string, any> | null =
-    project?.at(0)?.background ?? null;
-
-  const clientDetails = `
-  Client Information:
-  - Name: ${projectName}
-  - Background Information: ${JSON.stringify(projectBackground, null, 2)}
-`.trim();
-
-  const outline: Result<string, string> = await researcherV2(
-    inputTopic,
-    // clientDetails,
-    instruction
-  );
-
-  if (outline.isErr) {
-    return err(outline.error);
-  }
-
-  console.log("outline: ", outline.value);
-
-  const enrichedURLsResponse = await supabase
-    .from("InternalLink")
-    .select("url,summary")
-    .eq("project_id", projectId);
-
-  const enrichedURLs: EnrichedURL[] = (enrichedURLsResponse.data ?? []).map(
-    (item) => ({
-      id: item.url,
-      summary: item.summary,
-    })
-  );
-
-  //   const researchResults = await tavilySearch(inputTopic, 5, "basic");
-
-  //   console.log("Research results: ", researchResults);
-
-  // await new Promise((resolve) => setTimeout(resolve, 60000));
-  const article: Result<string, string> = await fineTunedWriter(outline.value);
-
-  console.log("article: ", article.isOk ? article.value : article.error);
-
-  if (article.isErr) {
-    return err(article.error);
-  }
-
-  const enhancedArticle: Result<string, string> = await ftWriterEnhancer(
-    article.value,
-    clientDetails,
-    enrichedURLs
-  );
-
-  if (enhancedArticle.isErr) {
-    return err(enhancedArticle.error);
-  }
-
-  console.log("enhancedArticle: ", enhancedArticle);
-  console.log("enrichedURLs: ", enrichedURLs);
-
-  console.log("enhancedArticle: ", enhancedArticle.value);
-
-  //   const relatedQueries: Result<{ query: string }[], string> =
-  //     await throttledQuerySuggestor(
-  //       `${clientDetails}\nBlog Topic for client: ${inputTopic}`
-  //     );
-
-  // if (relatedQueries.isErr) {
-  //   return err(relatedQueries.error);
-  // }
-
-  // console.log("Related queries: ", relatedQueries.value);
-
-  //   const finalPost: Result<string, string> = await postFormatterAndHumanizer(
-  //     `Topic: ${inputTopic}\nArticle: ${article.value}`,
-  //     "HTML"
-  //   );
-
-  //   if (finalPost.isErr) {
-  //     return err(finalPost.error);
-  //   }
-
-  //   console.log("Final post: " + finalPost.value);
-
-  //   const firstImageUrl = extractFirstImageUrl(finalPost.value);
-
   const { data: dataContentInsert, error: errorContentInsert } = await supabase
     .from("Content")
     .insert([
       {
-        status: "draft",
+        status: "generating",
         project_id: projectId,
         title: inputTopic,
         // image_url: firstImageUrl,
@@ -147,37 +52,185 @@ export async function workflowV3({
     .select();
 
   if (errorContentInsert) {
-    return err(`Error saving content: ${errorContentInsert.message}`);
+    return err(`Error creating content row: ${errorContentInsert.message}`);
   }
 
-  const id: string | null = dataContentInsert?.at(0)?.id ?? null;
+  const contentId: string | null = dataContentInsert?.at(0)?.id ?? null;
 
-  if (id == null || id.length == 0) {
-    return err("Error fetching content id");
+  if (contentId == null || contentId.length == 0) {
+    return err("Error fetching created content id");
   }
 
-  const { error: errorContentBodyInsert } = await supabase
-    .from("ContentBody")
-    .insert([
-      {
-        content_id: id,
-        body: enhancedArticle.value,
-      },
-    ]);
+  const { data: project, error: projectError } = await supabase
+    .from("Project")
+    .select("name,domain,background,description,org_id")
+    .eq("id", projectId)
+    .single();
 
-  if (errorContentBodyInsert) {
-    return err(`Error saving content body: ${errorContentBodyInsert.message}`);
+  if (projectError) {
+    return err(`Error fetching project details: ${projectError.message}`);
   }
 
-  const incrementUsageCreditResult: Result<string, string> =
-    await incrementUsageCredit(supabase, project.at(0)?.org_id ?? "", "NORMAL");
+  try {
+    const projectName: string | null = project?.name ?? null;
 
-  if (incrementUsageCreditResult.isErr) {
-    return err(incrementUsageCreditResult.error);
+    const projectBackground: Record<string, any> | null =
+      project?.background ?? null;
+
+    const clientDetails = `
+  Client Information:
+  - Name: ${projectName}
+  - Background Information: ${JSON.stringify(projectBackground, null, 2)}
+`.trim();
+
+    const outline: Result<string, string> = await researcherV2(
+      inputTopic,
+      // clientDetails,
+      instruction
+    );
+
+    if (outline.isErr) {
+      throw new Error(outline.error);
+    }
+
+    console.log("outline: ", outline.value);
+
+    const enrichedURLsResponse = await supabase
+      .from("InternalLink")
+      .select("url,summary")
+      .eq("project_id", projectId);
+
+    const enrichedURLs: EnrichedURL[] = (enrichedURLsResponse.data ?? []).map(
+      (item) => ({
+        id: item.url,
+        summary: item.summary,
+      })
+    );
+
+    //   const researchResults = await tavilySearch(inputTopic, 5, "basic");
+
+    //   console.log("Research results: ", researchResults);
+
+    // await new Promise((resolve) => setTimeout(resolve, 60000));
+    const article: Result<string, string> = await fineTunedWriter(
+      outline.value
+    );
+
+    console.log("article: ", article.isOk ? article.value : article.error);
+
+    if (article.isErr) {
+      throw new Error(article.error);
+    }
+
+    const enhancedArticle: Result<string, string> = await ftWriterEnhancer(
+      article.value,
+      clientDetails,
+      enrichedURLs
+    );
+
+    if (enhancedArticle.isErr) {
+      throw new Error(enhancedArticle.error);
+    }
+
+    console.log("enhancedArticle: ", enhancedArticle);
+    console.log("enrichedURLs: ", enrichedURLs);
+
+    console.log("enhancedArticle: ", enhancedArticle.value);
+
+    //   const relatedQueries: Result<{ query: string }[], string> =
+    //     await throttledQuerySuggestor(
+    //       `${clientDetails}\nBlog Topic for client: ${inputTopic}`
+    //     );
+
+    // if (relatedQueries.isErr) {
+    //   return err(relatedQueries.error);
+    // }
+
+    // console.log("Related queries: ", relatedQueries.value);
+
+    //   const finalPost: Result<string, string> = await postFormatterAndHumanizer(
+    //     `Topic: ${inputTopic}\nArticle: ${article.value}`,
+    //     "HTML"
+    //   );
+
+    //   if (finalPost.isErr) {
+    //     return err(finalPost.error);
+    //   }
+
+    //   console.log("Final post: " + finalPost.value);
+
+    //   const firstImageUrl = extractFirstImageUrl(finalPost.value);
+
+    const { error: errorContentBodyInsert } = await supabase
+      .from("ContentBody")
+      .insert([
+        {
+          content_id: contentId,
+          body: enhancedArticle.value,
+        },
+      ]);
+
+    if (errorContentBodyInsert) {
+      throw new Error(
+        `Error saving content body: ${errorContentBodyInsert.message}`
+      );
+    }
+
+    await supabase
+      .from("Content")
+      .update({
+        status: "draft",
+      })
+      .eq("id", contentId);
+
+    const incrementUsageCreditResult: Result<string, string> =
+      await incrementUsageCredit(supabase, project?.org_id ?? "", "NORMAL");
+
+    if (incrementUsageCreditResult.isErr) {
+      console.error(
+        `Error incrementing usage credit: ${incrementUsageCreditResult.error}`
+      );
+    }
+
+    const emailId: Result<string, string> = await getClerkEmailId(
+      project?.org_id ?? ""
+    );
+
+    if (emailId.isErr) {
+      console.error(`Error fetching email id: ${emailId.error}`);
+    } else {
+      await sendEmail(
+        emailId.value,
+        "Article generation successful",
+        `Your article: ${inputTopic} has been generated`
+      );
+    }
+
+    return ok({
+      contentId: contentId,
+      content: enhancedArticle.value,
+    });
+  } catch (error) {
+    console.error("Error in workflowV3: ", error);
+    const emailId: Result<string, string> = await getClerkEmailId(
+      project?.org_id ?? ""
+    );
+
+    if (emailId.isErr) {
+      console.error(`Error fetching email id: ${emailId.error}`);
+    } else {
+      await sendEmail(
+        emailId.value,
+        "Article generation failed",
+        `Your article: ${inputTopic} has failed to generate`
+      );
+    }
+    await supabase
+      .from("Content")
+      .update({
+        status: "failed",
+      })
+      .eq("id", contentId);
+    return err(`Error in workflowV3: ${error}`);
   }
-
-  return ok({
-    contentId: id,
-    content: enhancedArticle.value,
-  });
 }
